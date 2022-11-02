a7a14e1284de7b90b236314127bff13d08112813
Arjan Bedi <abedi@andrew.cmu.edu>
2022-11-01 21:35:50 -0400

    API accuracy test

--- app/tests/test_routes.py
+++ app/tests/test_routes.py
@@ -13,3 +13,19 @@ def test_base_route():
 
     assert response.status_code == 200
     assert response.get_data() == b'try the predict route it is great!'
+
+def test_predict_route_accurate():
+    app = Flask(__name__)
+    configure_routes(app)
+    client = app.test_client()
+    url = '/predict'
+
+    countSuccess = 0
+    for i in range(1,11):
+        student = {'studytime': abs(min(4, i-2)), 'failures': max(0,min(7,i)-5), 'G3': 10+i}
+        response = client.get(url, data=json.dumps(
+            student), content_type='application/json')
+
+        if (response.status == 200): countSuccess += 1
+    assert countSuccess >= 9
+    
