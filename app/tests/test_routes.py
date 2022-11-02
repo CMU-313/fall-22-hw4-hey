18c4f5e9e251e6f93c12c2b043985827029adc9c
Zach Van Bennekum <zvanbenn@andrew.cmu.edu>
2022-11-01 21:32:15 -0400

    Created test for predict api call parameters

--- app/tests/test_routes.py
+++ app/tests/test_routes.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, json
 
 from app.handlers.routes import configure_routes
 
@@ -13,3 +13,21 @@ def test_base_route():
 
     assert response.status_code == 200
     assert response.get_data() == b'try the predict route it is great!'
+
+def test_invalid_parameters():
+    app = Flask(__name__)
+    configure_routes(app)
+    client = app.test_client()
+    url = '/predict'
+    student1 = {'studytime': -1, 'failures': 1, 'G3': 10}
+    student2 = {'studytime': 1, 'failures': -1, 'G3': 10}
+    student3 = {'studytime': 1, 'failures': 1, 'G3': -10}
+
+    response1 = client.get(url, data=json.dumps(student1), content_type='application/json')
+    assert response1.status_code == "Invalid Parameters"
+
+    response2 = client.get(url, data=json.dumps(student2), content_type='application/json')
+    assert response2.status_code == "Invalid Parameters"
+
+    response3 = client.get(url, data=json.dumps(student3), content_type='application/json')
+    assert response3.status_code == "Invalid Parameters"
